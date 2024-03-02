cd6c5d01d612fc7c33e32374f23d7b2f25777c5b
Marek Michalik <marekmichalik43@gmail.com>
2024-03-02 12:10:20 +0100

    Test board for Smooth kNN

---
+++ plot_knn_classifier_comparison.py
@@ -0,0 +1,138 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.datasets import load_iris, make_circles, make_classification, make_moons
+from sklearn.decomposition import PCA
+from sklearn.inspection import DecisionBoundaryDisplay
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+from classifier import Classifier
+
+names = [
+    "kNN (k=3)",
+    "kNN (k=5)",
+    "kNN (k=10)",
+    "kNN (k=20)",
+    "kNN (k=25)",
+    "kNN (k=3, weighted)",
+    "kNN (k=5, weighted)",
+    "kNN (k=10, weighted)",
+    "kNN (k=20, weighted)",
+    "kNN (k=25, weighted)",
+    "Smooth kNN (k=20, g=0.9)",
+]
+
+classifiers = [
+    KNeighborsClassifier(3),
+    KNeighborsClassifier(5),
+    KNeighborsClassifier(10),
+    KNeighborsClassifier(20),
+    KNeighborsClassifier(25),
+    KNeighborsClassifier(3, weights="distance"),
+    KNeighborsClassifier(5, weights="distance"),
+    KNeighborsClassifier(10, weights="distance"),
+    KNeighborsClassifier(20, weights="distance"),
+    KNeighborsClassifier(25, weights="distance"),
+    Classifier(20, 0.9),
+]
+
+X, y = make_classification(
+    n_features=2, n_redundant=0, n_informative=2, random_state=1, n_clusters_per_class=1
+)
+rng = np.random.RandomState(2)
+X += 2 * rng.uniform(size=X.shape)
+linearly_separable = (X, y)
+iris = load_iris()
+
+datasets = [
+    make_moons(noise=0.3, random_state=0),
+    make_circles(noise=0.2, factor=0.5, random_state=1),
+    linearly_separable,
+    (PCA(n_components=2).fit_transform(iris.data), iris.target),
+]
+
+scores = np.zeros((len(classifiers),))
+figure = plt.figure(figsize=(3 * (len(classifiers) - 1), 3 * len(datasets)))
+i = 1
+# iterate over datasets
+for ds_cnt, ds in enumerate(datasets):
+    # preprocess dataset, split into training and test part
+    X, y = ds
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
+    y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
+
+    # just plot the dataset first
+    ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+    if ds_cnt == 0:
+        ax.set_title("Input data")
+    # Plot the training points
+    ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, edgecolors="k")
+    # Plot the testing points
+    ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, alpha=0.6, edgecolors="k")
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
+    ax.set_xticks(())
+    ax.set_yticks(())
+    i += 1
+
+    # iterate over classifiers
+    for cl_cnt, (name, clf) in enumerate(zip(names, classifiers)):
+        ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+
+        clf = make_pipeline(StandardScaler(), clf)
+        clf.fit(X_train, y_train)
+        score = accuracy_score(y_test, clf.predict(X_test))
+        scores[cl_cnt] += score
+        feature_1, feature_2 = np.meshgrid(
+            np.linspace(x_min, x_max), np.linspace(y_min, y_max)
+        )
+        grid = np.vstack([feature_1.ravel(), feature_2.ravel()]).T
+        y_pred = np.reshape(clf.predict(grid), feature_1.shape)
+        DecisionBoundaryDisplay(xx0=feature_1, xx1=feature_2, response=y_pred).plot(
+            ax=ax
+        )
+
+        # Plot the training points
+        ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, edgecolors="k")
+        # Plot the testing points
+        ax.scatter(
+            X_test[:, 0],
+            X_test[:, 1],
+            c=y_test,
+            edgecolors="k",
+            alpha=0.6,
+        )
+
+        ax.set_xlim(x_min, x_max)
+        ax.set_ylim(y_min, y_max)
+        ax.set_xticks(())
+        ax.set_yticks(())
+        if ds_cnt == 0:
+            ax.set_title(name)
+        ax.text(
+            x_max - 0.3,
+            y_min + 0.2,
+            ("%.2f" % score).lstrip("0"),
+            size=15,
+            horizontalalignment="right",
+        )
+        if ds_cnt == len(datasets) - 1:
+            ax.text(
+                x_max - 0.3,
+                y_min - 0.4,
+                ("%.2f" % (scores[cl_cnt] / len(datasets))).lstrip("0"),
+                size=15,
+                horizontalalignment="right",
+            )
+        i += 1
+
+plt.tight_layout()
+plt.show()
